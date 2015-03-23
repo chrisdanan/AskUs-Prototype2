a01009583eef62fa3e9a9f57223cb518996a152f
Christopher Danan <chrisddan@gmail.com>
2015-03-22 19:42:23 -0700

    Change value of JSON object "comments" to "results", and change value of "results" to an array instead of a single string.

--- server.js
+++ server.js
@@ -8,25 +8,25 @@ var express = require("express"),
 		{
 			"title" : "How do I make this work?",
 			"explanation" : "I want to make this project work, but I don't know where to start.  Can anyone help me please?",
-			"comments" : "null",
+			"responses" : [""],
 			"tag" : "career"
 		},
 		{
 			"title" : "Where is the best place to eat?",
 			"explanation" : "I'm in town for a few days.  Does anyone know a good place to grab dinner?",
-			"comments" : "null",
+			"responses" : [""],
 			"tag" : "food"
 		},
 		{
 			"title" : "How do you code in Javascript?",
 			"explanation" : "I'm new to Javascript.  Anybody have recommendations on what book to read?",
-			"comments" : "null",
+			"responses" : [""],
 			"tag" : "career"
 		},
 		{
 			"title" : "What should we name our pet dog?",
 			"explanation" : "We got a new Cocker Spaniel last night, but we can't think of a name.  She's female and is golden colored.  Any suggestions?",
-			"comments" : "null",
+			"responses" : [""],
 			"tag" : "life"
 		}
 	];
