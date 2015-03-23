291dd232d83fbef33be4bfc8d6bdc3d23cdd7c46
Christopher Danan <chrisddan@gmail.com>
2015-03-22 20:30:02 -0700

    Create more initial JSON question objects complete with responses so we can test them later.

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/*
+ *References:
+ 	-Quote from A Christmas Story
+ 		http://www.imdb.com/title/tt0085334/quotes
+*/
+
 "use strict";
 
 var express = require("express"),
@@ -20,14 +26,38 @@ var express = require("express"),
 		{
 			"title" : "How do you code in Javascript?",
 			"explanation" : "I'm new to Javascript.  Anybody have recommendations on what book to read?",
-			"responses" : [""],
+			"responses" : ["Javascript: The Good Parts by Douglas Crockford"],
 			"tag" : "career"
 		},
 		{
 			"title" : "What should we name our pet dog?",
 			"explanation" : "We got a new Cocker Spaniel last night, but we can't think of a name.  She's female and is golden colored.  Any suggestions?",
-			"responses" : [""],
+			"responses" : ["Cookiejar", "Princess", "Barbara Crimpleton"],
 			"tag" : "life"
+		},
+		{
+			"title" : "What car should I buy?",
+			"explanation" : "I'm a totally responsible 16 year old who has parents with pockets so deep you could fit Cthulhu in them.  What car should I get?",
+			"responses" : ["Jalopy", "Stick to bikes, kid"],
+			"tag" : "life"
+		},
+		{
+			"title" : "What present should I ask my parents for Christmas?",
+			"explanation" : "It's almost Christmas, and I need to start dropping hints to my folks!",
+			"responses" : ["an official Red Ryder, carbine action, two-hundred shot range model air rifle!", "Pink bunny pajamas", "New glasses"],
+			"tag" : "life"
+		},
+		{
+			"title" : "Where should we go for our family vacation?",
+			"explanation" : "The missus and I finally have vacation days from the plant; where should we take our little boy Jimmy?",
+			"responses" : ["Disneyland, duh", "How about mini golfing?", "Go hunting and teach Jimmy how to be a man!!!", "First"],
+			"tag" : "travel"
+		},
+		{
+			"title" : "I need gurl 4 prom lolz",
+			"explanation" : "Yo, I need a gurl 4 prom - give me yo digits!!11!",
+			"responses" : ["no", "NO", "No", "...no"],
+			"tag" : "relationships"
 		}
 	];
 
